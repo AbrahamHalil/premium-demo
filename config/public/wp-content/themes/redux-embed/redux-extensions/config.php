5e0fe6ad32e567fe0b6590d7ab3c080e89c570e2
Dovy Paukstys <info@simplerain.com>
2014-01-10 03:16:11 +0100

    Metaboxes

--- config/public/wp-content/themes/redux-embed/redux-extensions/config.php
+++ config/public/wp-content/themes/redux-embed/redux-extensions/config.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__).'/loader.php');
 
 
 
-if ( !function_exists( "redux_add_widget_areas" ) ) {
+if ( !function_exists( "redux_add_widget_areas" ) ):
     function redux_add_widget_areas($widget_areas) {
 
         $widget_areas[] = "Custom Sidebar 1";
@@ -18,4 +18,53 @@ if ( !function_exists( "redux_add_widget_areas" ) ) {
         return $widget_areas;
     }
     add_action('redux/widget_areas', 'redux_add_widget_areas');
-}
+endif;
+
+
+
+if ( !function_exists( "redux_add_metaboxes" ) ):
+  function redux_add_metaboxes($metaboxes) {
+      
+      $boxSections[] = array(
+        'title' => __('Home Settings', 'redux-framework-demo'),
+        'header' => __('Welcome to the Simple Options Framework Demo', 'redux-framework-demo'),
+        'desc' => __('Redux Framework was created with the developer in mind. It allows for any theme developer to have an advanced theme panel with most of the features a developer would need. For more information check out the Github repo at: <a href="https://github.com/ReduxFramework/Redux-Framework">https://github.com/ReduxFramework/Redux-Framework</a>', 'redux-framework-demo'),
+        'icon_class' => 'icon-large',
+          'icon' => 'el-icon-home',
+          // 'submenu' => false, // Setting submenu to false on a given section will hide it from the WordPress sidebar menu!
+        'fields' => array(
+          array(
+            'title'     => __( 'Layout', 'redux-framework-demo' ),
+            'desc'      => __( 'Select main content and sidebar arrangement. Choose between 1, 2 or 3 column layout.', 'redux-framework-demo' ),
+            'id'        => 'layout',
+            'default'   => 1,
+            'type'      => 'image_select',
+            'customizer'=> array(),
+            'options'   => array( 
+              0         => ReduxFramework::$_url . 'assets/img/1c.png',
+              1         => ReduxFramework::$_url . 'assets/img/2cr.png',
+              2         => ReduxFramework::$_url . 'assets/img/2cl.png',
+              3         => ReduxFramework::$_url . 'assets/img/3cl.png',
+              4         => ReduxFramework::$_url . 'assets/img/3cr.png',
+              5         => ReduxFramework::$_url . 'assets/img/3cm.png',
+            )
+          )
+        )
+      );
+  	  $metaboxes = array();
+      $metaboxes[] = array(
+        'id' => 'demo-layout',
+        //'title' => __('Cool Options', 'redux-framework-demo'),
+        'post_types' => array('page','post'),
+        'position' => 'side', // normal, advanced, side
+        'priority' => 'high', // high, core, default, low
+        'sections' => $boxSections
+      );
+
+      // Kind of overkill, but ahh well.  ;)
+      //$metaboxes = apply_filters( 'your_custom_redux_metabox_filter_here', $metaboxes );
+
+  	return $metaboxes;
+  }
+  add_action('redux/metaboxes/'.$redux_opt_name.'/boxes', 'redux_add_metaboxes');
+endif;
